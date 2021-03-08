fb8c9464d81c15ce855493aa5413219a2bbd352a
Vighnesh Natarajan Ganesh <ngvighnesh@gmail.com>
2021-03-08 19:39:42 +0530

    imporoved naming

--- src/ParDepPlot.jl
+++ src/ParDepPlot.jl
@@ -1,43 +1,43 @@
 module ParDepPlot
 
-function PDP(mach::Machine, df::DataFrame, targetColumn::String, targets::Array{String})
+function PDPCategorical(mach::Machine, df::DataFrame, depColumn::String, categories::Vector{String})
         #=
         This function returns a 1-D PDP plot of multi-class-classification model
 
         Arguments:
         mach: The machine (the wrapping the model in data)
         df: The dataframe containing the test data
-        targetColumn: The feature for which PDP plot must be drawn
-        targets: The different classes of the targetColumn for which plot must be generated
+        depColumn: The feature for which PDP plot must be drawn
+        categories: The different classes of the targetColumn for which plot must be generated
 
         Returns:
-        a plot with separate subplots for each class mentioned in targets argument
+        a plot with separate subplots for each category mentioned in categories argument
         =#
 
         # cross product the target feature for PDP along with non-target features to get data
-        dfCross = crossjoin( DataFrame(targetColumn=df[!, targetColumn]), df[!, filter(x->x!=targetColumn, names(df))] )
+        dfCross = crossjoin( DataFrame(depColumn=df[!, depColumn]), df[!, filter(x->x!=depColumn, names(df))] )
 
         # rename the column from the symbol targetColumn to string in the variable targetColumn
-        colnames = pushfirst!(setdiff(names(dfCross), ["targetColumn"]), targetColumn)
+        colnames = pushfirst!(setdiff(names(dfCross), ["depColumn"]), depColumn)
         rename!(dfCross, Symbol.(colnames))
 
         # make the predictions
         yhat = predict(mach, dfCross);
-        for x in targets
+        for x in categories
                 dfCross[!, x] = broadcast(pdf, yhat, x)
         end
 
-        # group by the targetColumn
-        groupedDfCross = groupby(dfCross, targetColumn)
+        # group by the depColumn
+        groupedDfCross = groupby(dfCross, depColumn)
 
         #initialize the plot
-        p=plot(layout=length(targets))
+        p=plot(layout=length(categories))
 
         # generate the statistics for every class in target column and make the subplot
-        for i in 1:length(targets)
-                meanYHat = combine(groupedDfCross,targets[i]=>mean=>"mean")
-                sort!(meanYHat, targetColumn) # to make sure the the db is in ascending order of target var
-                plot!(p, meanYHat[!, targetColumn], meanYHat[!, "mean"], subplot=i, title=targets[i], label="")
+        for i in 1:length(categories)
+                meanYHat = combine(groupedDfCross,categories[i]=>mean=>"mean")
+                sort!(meanYHat, depColumn) # to make sure the the db is in ascending order of target var
+                plot!(p, meanYHat[!, depColumn], meanYHat[!, "mean"], subplot=i, title=string(categories[i]))
         end
 
         # return the subplot
